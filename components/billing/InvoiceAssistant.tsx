92cb67c47fd6826bdf7a966e970388c40461eced
Joshua <joshuaborromeo@Mac.lan>
2025-05-16 16:41:34 +0530

    chore: upgrade react-query to v5 and use isPending

--- components/billing/InvoiceAssistant.tsx
+++ components/billing/InvoiceAssistant.tsx
@@ -68,10 +68,7 @@ export function InvoiceAssistant() {
     enabled: false, // Only fetch when explicitly requested
   });
 
-  // @ts-ignore – FIXME migrate to React-Query v5 and usePending
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const { mutate: generateInvoice, data: invoiceData, isLoading: isGeneratingInvoice } = useMutation({
+  const { mutate: generateInvoice, data: invoiceData, isPending: isGeneratingInvoice } = useMutation({
     mutationFn: generateInvoiceDraft,
   });
 
